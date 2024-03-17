41e23cf7c99f06aafbc32aec941286e77c8ec93f
Riya Singh <singhriyaa006@gmail.com>
2024-03-17 10:44:58 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -204,15 +204,17 @@ class HomePage extends StatelessWidget{
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          leadingWidth: 70,
+          leadingWidth: 385,
           backgroundColor:  Color.fromARGB(239, 0, 0, 0),
-          leading:Padding(padding: EdgeInsets.only(left: 7),child:Image.asset('assets/images/imbc.png',)),
-          
-          title: Icon(
-              Icons.settings,
-              color: const Color.fromARGB(255, 155, 24, 14),
-              size: 35
-            ),
+          leading:Row(children:[
+            Image.asset('assets/images/imbc.png',height: 40,),
+            Spacer(),
+            Icon(
+                Icons.person_sharp,
+                color: const Color.fromARGB(255, 155, 24, 14),
+                size: 37
+              ),
+          ])
           ),
         body: SingleChildScrollView(
           child: Container(
@@ -220,8 +222,206 @@ class HomePage extends StatelessWidget{
             height: 790,
             decoration: BoxDecoration(color: Color.fromARGB(239, 0, 0, 0)),
             padding: EdgeInsets.all(12),
+            child:Column(
+              children: [
+              TextField(
+                decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search,color: Color.fromARGB(255, 174, 11, 11),),
+                hintText: 'Actor, title or genre',
+                filled: true,
+                fillColor: Color.fromARGB(184, 255, 255, 255),
+                border: OutlineInputBorder(borderSide: BorderSide.none,)
+                ,suffixIcon: Icon(Icons.mic,color: Color.fromARGB(255, 174, 11, 11),)
+                )
+                ,),
+            //   SingleChildScrollView(
+            //   scrollDirection: Axis.horizontal,
+            //   child: Row(children: [
+
+            //   ],)
+            // ),
+            SizedBox(height: 20,),
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text('Genres', style: TextStyle(color: Color.fromARGB(255, 174, 11, 11),fontSize: 28,fontFamily: 'LeagueSpartan',fontWeight: FontWeight.w800),textAlign: TextAlign.right,)
+              ),
+              SizedBox(height: 10,),
+              Row(children: [
+                SizedBox(width: 5,),
+                ElevatedButton(onPressed: ()=>(), child: Text('Comedy'),
+              style: ElevatedButton.styleFrom(minimumSize: Size(170, 60),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),
+              ),
+              backgroundColor:Color.fromARGB(144, 59, 59, 59),
+              foregroundColor: Color.fromARGB(255, 255, 255, 255)
+              )),
+              SizedBox(width: 20,),
+              ElevatedButton(onPressed: ()=>(), child: Text('Drama'),
+              style: ElevatedButton.styleFrom(minimumSize: Size(170, 60),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),
+              ),
+              backgroundColor:Color.fromARGB(144, 59, 59, 59),
+              foregroundColor: Color.fromARGB(255, 255, 255, 255)
+              )),
+              
+              ],),
+              SizedBox(height: 15,),
+              Row(children: [
+                SizedBox(width: 5,),
+                ElevatedButton(onPressed: ()=>(), child: Text('Romance'),
+              style: ElevatedButton.styleFrom(minimumSize: Size(170, 60),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),
+              ),
+              backgroundColor:Color.fromARGB(144, 59, 59, 59),
+              foregroundColor: Color.fromARGB(255, 255, 255, 255)
+              )),
+              SizedBox(width: 20,),
+              ElevatedButton(onPressed: ()=>(), child: Text('Action'),
+              style: ElevatedButton.styleFrom(minimumSize: Size(170, 60),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),
+              ),
+              backgroundColor:Color.fromARGB(144, 59, 59, 59),
+              foregroundColor: Color.fromARGB(255, 255, 255, 255)
+              )),
+              
+              ],),
+              SizedBox(height: 15,),
+              Row(children: [
+                SizedBox(width: 5,),
+                ElevatedButton(onPressed: ()=>(), child: Text('Suspense'),
+              style: ElevatedButton.styleFrom(minimumSize: Size(170, 60),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),
+              ),
+              backgroundColor:Color.fromARGB(144, 59, 59, 59),
+              foregroundColor: Color.fromARGB(255, 255, 255, 255)
+              )),
+              SizedBox(width: 20,),
+              ElevatedButton(onPressed: ()=>(), child: Text('Anime'),
+              style: ElevatedButton.styleFrom(minimumSize: Size(170, 60),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12),
+              ),
+              backgroundColor:Color.fromARGB(144, 59, 59, 59),
+              foregroundColor: Color.fromARGB(255, 255, 255, 255)
+              )),
+              
+              ],),
+              SizedBox(height: 10,),  
+              Text('See more ˅',style:TextStyle(color: Color.fromARGB(255, 174, 11, 11),fontSize: 16),)
+              ,SizedBox(height: 25,),
+              Align(
+              alignment: Alignment.topLeft,
+              child: Text('Featured Collections', style: TextStyle(color: Color.fromARGB(255, 174, 11, 11),fontSize: 28,fontFamily: 'LeagueSpartan',fontWeight: FontWeight.w800),textAlign: TextAlign.right,)
+              ),
+              SizedBox(height: 15,),
+             Container(
+                child:Row(children:[
+                  
+            Text('Hindi',style: TextStyle(color: Color.fromARGB(168, 174, 11, 11),fontSize: 20,),),
+            Spacer(),
+            Icon(
+                Icons.arrow_forward_ios,
+                color: Color.fromARGB(137, 155, 23, 11),
+                size: 20
+              ),
+              
+          ])
+             ),
+              SizedBox(height: 15,),
+             Container(
+                child:Row(children:[
+                  
+            Text('English',style: TextStyle(color:Color.fromARGB(168, 174, 11, 11),fontSize: 20),),
+            Spacer(),
+            Icon(
+                Icons.arrow_forward_ios,
+                color:Color.fromARGB(168, 174, 11, 11),
+                size: 20
+              ),
+              
+          ])
+             ),
+             SizedBox(height: 15,),
+             Container(
+                child:Row(children:[
+                  
+            Text('Telugu',style: TextStyle(color:Color.fromARGB(168, 174, 11, 11),fontSize: 20),),
+            Spacer(),
+            Icon(
+                Icons.arrow_forward_ios,
+                color: Color.fromARGB(168, 174, 11, 11),
+                size: 20
+              ),
+              
+          ])
+             ),
+             SizedBox(height: 15,),
+             Container(
+                child:Row(children:[
+                  
+            Text('Punjabi',style: TextStyle(color:Color.fromARGB(168, 174, 11, 11),fontSize: 20),),
+            Spacer(),
+            Icon(
+                Icons.arrow_forward_ios,
+                color: Color.fromARGB(168, 174, 11, 11),
+                size: 20
+              ),
+              
+          ])
+             ),
+             SizedBox(height: 15,),
+             Container(
+                child:Row(children:[
+                  
+            Text('Malyalam',style: TextStyle(color:Color.fromARGB(168, 174, 11, 11),fontSize: 20),),
+            Spacer(),
+            Icon(
+                Icons.arrow_forward_ios,
+                color:Color.fromARGB(168, 174, 11, 11),
+                size: 20
+              ),
+              
+          ])
+             ),
+             SizedBox(height: 15,),
+             Container(
+                child:Row(children:[
+                  
+            Text('Urdu',style: TextStyle(color: Color.fromARGB(168, 174, 11, 11),fontSize: 20),),
+            Spacer(),
+            Icon(
+                Icons.arrow_forward_ios,
+                color: Color.fromARGB(168, 174, 11, 11),
+                size: 20
+              ),
+              
+          ])
+             ),
+             SizedBox(height: 15,),
+             Container(
+                child:Row(children:[
+                  
+            Text('Japanese',style: TextStyle(color: Color.fromARGB(168, 174, 11, 11),fontSize: 20),),
+            Spacer(),
+            Icon(
+                Icons.arrow_forward_ios,
+                color: Color.fromARGB(168, 174, 11, 11),
+                size: 20
+              ),
+              
+          ])
+             )
+              ],
+            )
+            
           ),
         ),
+        bottomNavigationBar: BottomNavigationBar(items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home',),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(icon: Icon(Icons.download_done_sharp), label: 'Watchlist'),
+
+        ],selectedIconTheme: IconThemeData(color: Color.fromARGB(255, 174, 11, 11)),selectedLabelStyle: TextStyle(color:Color.fromARGB(255, 174, 11, 11) ),
+        backgroundColor: Color.fromARGB(255, 151, 148, 148),),
       ),
     );
   }
