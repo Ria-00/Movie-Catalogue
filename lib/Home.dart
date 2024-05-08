638670e26d7d05654fb2827a629d9e22d46865cd
Riya Singh <singhriyaa006@gmail.com>
2024-05-08 14:29:31 +0530

    updated

--- lib/Home.dart
+++ lib/Home.dart
@@ -2,7 +2,9 @@ import 'package:curved_labeled_navigation_bar/curved_navigation_bar.dart';
 import 'package:curved_labeled_navigation_bar/curved_navigation_bar_item.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_catalogue/HomePage.dart';
+import 'package:movie_catalogue/Profile.dart';
 import 'package:movie_catalogue/SearchPage.dart';
+import 'package:movie_catalogue/SignUp.dart';
 import 'package:movie_catalogue/WatchList.dart';
 import 'package:movie_catalogue/main.dart';
 
@@ -18,7 +20,7 @@ class Home extends StatefulWidget{
 class HomeState extends State<Home>{
 
   int _page=0;
-
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   List<Widget> _pages=[HomePage(),SearchPage(),WatchList()];
 
   @override
@@ -26,26 +28,26 @@ class HomeState extends State<Home>{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+      key: _scaffoldKey,
+      drawer: _buildDrawer(),
         appBar: AppBar(
           leadingWidth: 380,
-          backgroundColor:  Color.fromARGB(239, 0, 0, 0),
-          leading:Row(children:[
-            SizedBox(width: 10,),
-            Image.asset('assets/images/imbc.png',height: 40,),
-            Spacer(),
-            Icon(
-                Icons.person_sharp,
+          backgroundColor: Color.fromARGB(239, 0, 0, 0),
+          leading: Row(
+            children: [
+              SizedBox(width: 10),
+              Image.asset('assets/images/imbc.png',height: 40,),
+              Spacer(),
+              IconButton(
+                icon: Icon(Icons.person),
+                iconSize: 38, 
                 color: const Color.fromARGB(255, 155, 24, 14),
-                size: 37
+                onPressed: () => _scaffoldKey.currentState!.openDrawer(),
               ),
-              SizedBox(width: 8,),
-            GestureDetector(
-              child: Text('Sign Out',style: TextStyle(color: const Color.fromARGB(255, 155, 24, 14) ,fontFamily: 'LeagueSpartan',fontSize: 18,fontWeight: FontWeight.w800),),
-              onTap: ()=>Navigator.push(context, MaterialPageRoute(builder: (context) =>MyApp())),
-            )
-            
-          ])
+
+            ],
           ),
+        ),
         body: _pages[_page],
         bottomNavigationBar: CurvedNavigationBar(
             onTap: (index){
@@ -63,6 +65,31 @@ class HomeState extends State<Home>{
             CurvedNavigationBarItem(child: Icon(Icons.search),),
             CurvedNavigationBarItem(child: Icon(Icons.download_done_sharp),),
           ]),
+      ),
+    );
+  }
+
+    Widget _buildDrawer() {
+    return Drawer(
+      backgroundColor: Color.fromARGB(234, 8, 8, 8),
+      child: ListView(
+        children: [
+          UserAccountsDrawerHeader(
+            decoration: BoxDecoration(color: Color.fromARGB(255, 118, 15, 15)),
+            accountName: Text('RIYA SINGH',style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),), 
+            accountEmail: Text('singhriya@gmail.com',style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),),
+            currentAccountPicture: Image.network("https://static.vecteezy.com/system/resources/previews/023/410/729/original/animal-dog-3d-icon-png.png"),),
+          ListTile(
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => SignUp ())),
+            title: Text('New Account',style: TextStyle(color: Colors.white)),
+            // style:,
+          ),
+          // Add more menu items here
+          ListTile(
+            title: Text('Sign Out',style: TextStyle(color: Colors.white),),
+            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp())),
+          ),
+        ],
       ),
     );
   }
